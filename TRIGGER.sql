8cc2535dcb864867261b94ec17f1d528e22be967
André <108432366+buenolinux@users.noreply.github.com>
2022-08-02 14:05:06 -0300

    Update TRIGGER.sql

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -2,6 +2,7 @@ USE fluxocaixa;
 /*
 Vamos criar uma trigger para atualizar a tabela
 caixa a cada venda feita
+É uma boa prática usar o prefixo tgr nos nomes das trigger
 */
 
 CREATE TRIGGER tgr_VendaInserida
@@ -22,7 +23,22 @@ SELECT * FROM tab_vendas;
 /* Vamos inserir uma venda */
 
 INSERT INTO tab_vendas (data, valor)
-VALUES ('2022-08-02',50);
-
-INSERT INTO tab_vendas (data, valor)
 VALUES ('2022-08-02',1272);
+
+/*
+Vamos criar uma trigger para atualizar a tabela
+caixa a cada venda excluída
+*/
+
+CREATE TRIGGER tgr_VendaExcluida
+ON tab_vendas
+FOR DELETE 
+AS
+BEGIN 
+	DECLARE
+	@var_valor DECIMAL(10,2),
+	@var_data DATE
+	SELECT @var_data = data, @var_valor = valor FROM DELETED
+	UPDATE tab_caixa SET saldo_final = saldo_final - @var_valor
+	WHERE data = @var_data
+END;
